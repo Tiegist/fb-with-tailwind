3cdbdcdf7f4b1cd6402dfbbc332aca449d02ef98
Tiegist Girma <tiegistgirma40@gmail.com>
2024-05-24 14:42:39 -0700

    fb v3

--- fbv3/src/components/grouppage.jsx
+++ fbv3/src/components/grouppage.jsx
@@ -20,17 +20,17 @@ function Grouppage(){
 return(
     <div>
   
-    <div className='thewholeconatainer'>
+    <div className='w-96'>
 <div className='page2leftcontainer'>
     <div className='flex'>
         <div className='text-2xl mr-32 ml-2'><h3>Marketlace</h3></div>
         <div className='text-xl mt-2 hidden sm:block'><IoIosSettings className='settingsubicon'/></div>
     </div>
     <div className='mb-6 ml-6'>
-        <CiSearch className='ml-64 text-xl -mt-6'/>
-        <input className='hidden sm:block' type='text' placeholder='search Marketplace'></input>
+        <CiSearch className='ml-80 text-xl -mt-6'/>
+        <input className='hidden sm:block md:block' type='text' placeholder='search Marketplace'></input>
     </div>
-    <div className='hidden sm:block'>
+    <div className='hidden sm:block md:block'>
     <div className='browseallclass'>
         <PiBrowsersFill className='browseallicon'/>
         <input className='browseinput' type='text' placeholder='Browse all Marketplace'></input>
@@ -64,18 +64,18 @@ return(
     
 
     </div>
-    <div className='flex mb-3'>
-        <div className='ml-6 bg-gray-200 p-2 rounded-full'><IoPersonSharp  /></div>
+    <div className='flex mb-3 '>
+        <div className='ml-2 bg-gray-200 text-sm p-2 rounded-full mr-2'><IoPersonSharp  /></div>
         
-        <button className='ml-2 text-sm bg-gray-200 p-1 w-14 rounded-2xl font-semibold'>Inbox</button>
-        <button className='ml-2 bg-gray-200  w-12 rounded-2xl font-semibold'>Sell</button>
-        <button className='ml-2 bg-gray-200  w-18 rounded-2xl font-semibold'>All Categories</button>
-        <button className='ml-2 bg-gray-200  w-16 rounded-2xl font-semibold'>Search</button>
+        <button className='ml-2 text-sm bg-gray-200 p-1 w-14 rounded-2xl font-semibold mr-2'>Inbox</button>
+        <button className='ml-2 text-sm bg-gray-200 p-1 w-12 rounded-2xl font-semibold mr-2'>Sell</button>
+        <button className='ml-2 text-sm bg-gray-200 p-1 w-18 rounded-2xl font-semibold mr-2'>Categories</button>
+        <button className='ml-2 text-sm bg-gray-200 p-1 w-16 rounded-2xl font-semibold mr-2'>Search</button>
     </div>
            
     <hr></hr>
     <div className='flex mt-3 mb-3'>
-    <div className='mr-20 ml-2 font-semibold text-md'><h4 >today's picks</h4></div>
+    <div className='mr-12 ml-2 font-semibold text-md'><h4 >today's picks</h4></div>
     <div className='flex'>
         <IoLocation  className='text-blue-800 mr-2'/>
         <p className='text-blue-800 text-md font-semibold'>Kombolcha . 65 Kilometers</p></div>
@@ -177,67 +177,63 @@ return(
     <div className=''>
         <p className='Habesha kemis'></p>
     </div>
-   <div className='flex'>
-    <div className=''>
+   <div className='flex mt-4'>
+    <div className='ml-1 mr-1 rounded-sm w-1/2'>
         <img className='' src='\amarech-ethiopian-traditional.webp'></img>
-        <p className=''>Amarech Ethiopian</p>
-        <p className=''>20000 Birr</p>
+        < a href='www.google.com' className=''>Amarech Ethiopian</ a>
+        < a a href='www.google.com' className=''>20000 Birr</ a>
     </div>
-    <div>
+    <div className='mr-1 w-1/2'>
         <img src='\raya-dress.webp'></img>
-        <p>Ethiopian Raya</p>
-        <p>11000</p>
+        < a a href='www.google.com'>Ethiopian Raya</ a>
+        < a a href='www.google.com'>11000</ a>
     </div>
    </div>
 
 
    <div className='flex'>
-    <div className=''>
+    <div className='wl-1 mr-1 w-1/2'>
         <img src='\danayt3_1600x.webp'></img>
-        <p className=''>Amarech Ethiopian</p>
-        <p className=''>20000 Birr</p>
+        < a a href='www.google.com' className=''>Holiday Habesha</ a>
+        < a a href='www.google.com' className=''>17600 Birr</ a>
     </div>
-    <div>
+    <div className='mr-1 w-1/2'>
         <img src='\Burgendy-Crowning-Kaba.jpg'></img>
-        <p>Ethiopian Raya</p>
-        <p>11000</p>
+        < a a href='www.google.com'>Burgendy-crowning-kaba</ a>
+        < a a href='www.google.com'>21000</ a>
     </div>
    </div>
 
 
    <div className='flex'>
-    <div className=''>
+    <div className='mr-1 ml-1 w-1/2'>
         <img src='\raya-dress.webp'></img>
-        <p className=''>Amarech Ethiopian</p>
-        <p className=''>20000 Birr</p>
+        < a a href='www.google.com' className=''>Rayan dress</ a>
+        < a a href='www.google.com' className=''>8900 Birr</ a>
     </div>
-    <div>
+    <div className='mr-1 w-1/2'>
         <img src='\Royal-Habesha-Kemis-3.jpg'></img>
-        <p>Ethiopian Raya</p>
-        <p>11000</p>
+        < a a href='www.google.com'>Royal Habesha kemis</ a>
+        < a a href='www.google.com'>19200</ a>
     </div>
    </div>
 
    <div className='flex'>
-    <div className=''>
+    <div className='mr-1 ml-1 w-1/2'>
         <img src='\dress-with- baloon.webp'></img>
-        <p className=''>Amarech Ethiopian</p>
-        <p className=''>20000 Birr</p>
+        < a a href='www.google.com' className=''>modern Ethiopian</ a>
+        < a a href='www.google.com' className=''>17300 Birr</ a>
     </div>
-    <div>
+    <div className='mr-1 ml-1 w-1/2'>
         <img src='\fnan-habesha-kemis.webp'></img>
-        <p>Ethiopian Raya</p>
-        <p>11000</p>
+        < a a href='www.google.com'>Casual Ethiopian</ a>
+        < a a href='www.google.com'>15200</ a>
     </div>
    </div>
   
     </div>
     
-    <div className='shoes'>
-        <p>shoes</p>
-    <img className='\300$shoe.webp'></img>
-    <p>shoe</p>
-   </div>
+    
     </div>
    
 
